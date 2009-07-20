551713cdfaf2ce6504c3f7fc1e45d9e605914bd6
Kenny Root <kenny@the-b.org>
2009-07-19 19:49:18 -0500

    Avoid recipient duplication in Messaging
    If you try to send an SMS/MMS to a contact which has a comma in
    their name field, the Messaging app will add a duplicate recipient
    for each of the commas in their name.
    
    The RecipientsEditor tokenizer only scanned directly for commas
    and then looked at the spans overlapping with those regions. This
    change will take into account the span end each time and adjust the
    current index to be at the end of the span if necessary.
    
    Fixes http://code.google.com/p/android/issues/detail?id=2739

--- src/com/android/mms/ui/RecipientsEditor.java
+++ src/com/android/mms/ui/RecipientsEditor.java
@@ -168,12 +168,18 @@ public class RecipientsEditor extends MultiAutoCompleteTextView {
         String bcc = getAnnotation(a, "bcc");
         String number = getAnnotation(a, "number");
 
+        int displayEnd = -1;
+        if (a.length > 0) {
+            displayEnd = sp.getSpanEnd(a[0]);
+        }
+
         Recipient r = new Recipient();
 
         r.name = name;
         r.label = label;
         r.bcc = bcc.equals("true");
         r.number = TextUtils.isEmpty(number) ? TextUtils.substring(sp, start, end) : number;
+        r.displayEnd = displayEnd;
         
         if (TextUtils.isEmpty(r.name) && Mms.isEmailAddress(r.number)) {
             ContactInfoCache cache = ContactInfoCache.getInstance();
@@ -252,6 +258,10 @@ public class RecipientsEditor extends MultiAutoCompleteTextView {
                         Recipient r = getRecipientAt(sp, start, i, mContext);
 
                         rl.add(r);
+
+                        if (r.displayEnd > i) {
+                            i = r.displayEnd;
+                        }
                     }
 
                     i++;
