67ec6c54e11c19caf894b4ffce7250fb3fd96d30
Wei Huang <weih@google.com>
2009-11-05 22:23:48 -0800

    bug #2236553: catch NPE thrown by SmsManager.sendMultipartTextMessage().
    - need to catch the NPE and deal with it, like clean up and delete the queued message. Otherwise we not only get a crash in the app, but the queued message would keep getting retried, therefore grow the conversation thread everytime it retries.
    
    - also catch a RuntimeException when we try to insert an existing conversation into the Conversation cache. That caused an app crash too!
    
    Change-Id: Iba081955e19d7a5eb517c3a3aea5186534787c15

--- src/com/android/mms/data/Conversation.java
+++ src/com/android/mms/data/Conversation.java
@@ -131,7 +131,12 @@ public class Conversation {
             long threadId = getOrCreateThreadId(context, recipients);
             conv = new Conversation(context, threadId);
 
-            Cache.put(conv);
+            try {
+                Cache.put(conv);
+            } catch (IllegalStateException e) {
+                LogTag.error("Tried to add duplicate Conversation to Cache");
+            }
+
             return conv;
         }
     }
