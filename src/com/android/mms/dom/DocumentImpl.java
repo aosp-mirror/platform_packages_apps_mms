2a682490e6caa0b2fc5793e0881e6ba85c21b66d
Jesse Wilson <jessewilson@google.com>
2010-01-26 18:18:16 -0800

    Bringing MMS up-to-date with Dalvik's new DOM v3 support.
    See related Dalvik change, https://android-git.corp.google.com/g/38533

--- src/com/android/mms/dom/DocumentImpl.java
+++ src/com/android/mms/dom/DocumentImpl.java
@@ -20,6 +20,7 @@ package com.android.mms.dom;
 import org.w3c.dom.Attr;
 import org.w3c.dom.CDATASection;
 import org.w3c.dom.Comment;
+import org.w3c.dom.DOMConfiguration;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
@@ -140,5 +141,54 @@ public abstract class DocumentImpl extends NodeImpl implements Document {
     public String getNodeName() {
         // The value of nodeName is "#document" when Node is a Document
         return "#document";
+    }
+
+    public String getInputEncoding() {
+        return null;
+    }
+
+    public String getXmlEncoding() {
+        return null;
+    }
+
+    public boolean getXmlStandalone() {
+        return false;
+    }
+
+    public void setXmlStandalone(boolean xmlStandalone) throws DOMException {}
+
+    public String getXmlVersion() {
+        return null;
+    }
+
+    public void setXmlVersion(String xmlVersion) throws DOMException {}
+
+    public boolean getStrictErrorChecking() {
+        return true;
+    }
+
+    public void setStrictErrorChecking(boolean strictErrorChecking) {}
+
+    public String getDocumentURI() {
+        return null;
+    }
+
+    public void setDocumentURI(String documentURI) {}
+
+    public Node adoptNode(Node source) throws DOMException {
+        throw new DOMException(DOMException.NOT_SUPPORTED_ERR, null);
+    }
+
+    public DOMConfiguration getDomConfig() {
+        throw new DOMException(DOMException.NOT_SUPPORTED_ERR, null);
+    }
+
+    public void normalizeDocument() {
+        throw new DOMException(DOMException.NOT_SUPPORTED_ERR, null);
+    }
+
+    public Node renameNode(Node n, String namespaceURI, String qualifiedName)
+            throws DOMException {
+        throw new DOMException(DOMException.NOT_SUPPORTED_ERR, null);
     }
 }
