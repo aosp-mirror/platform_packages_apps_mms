37822903cd7f9b3d082cd19dbd882f4291a364a7
Naveen Kalla <nkalla@codeaurora.org>
2010-03-23 07:35:11 -0700

    Type Zero Sms should not be displayed/stored/notified.
    Type Zero messages indicated by TP_PID field set to value 0x40,
    should not be displayed/stored/notified. They should only be
    acknowledged.

--- src/com/android/mms/transaction/SmsReceiverService.java
+++ src/com/android/mms/transaction/SmsReceiverService.java
@@ -330,6 +330,16 @@ public class SmsReceiverService extends Service {
     private Uri insertMessage(Context context, SmsMessage[] msgs) {
         // Build the helper classes to parse the messages.
         SmsMessage sms = msgs[0];
+        final int TYPE_ZERO_MSG = 0x40;
+
+        // As per 3GPP TS 23.040 section 9.2.3.9, Type Zero messages, indicated by TP_PID
+        // field set to value 0x40, should not be Displayed/Stored/Notified. They should
+        // only be acknowledged which is done in the frameworks. For CDMA,
+        // getProtocolIdentifier() returns zero, hence this code will not be executed.
+        if (sms.getProtocolIdentifier() == TYPE_ZERO_MSG) {
+            Log.i(TAG, "Received Type Zero Message, Ignoring It");
+            return null;
+        }
 
         if (sms.getMessageClass() == SmsMessage.MessageClass.CLASS_0) {
             displayClassZeroMessage(context, sms);
