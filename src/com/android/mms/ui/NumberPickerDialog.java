60161290654b704c0e9da25ed34250fd86305a72
Christian Mehlmauer <FireFart@gmail.com>
2010-05-17 20:29:16 +0200

    removed calls to deprecated APIs
    Change-Id: Iaaee3e770bf65d4f041b0786d0cf6c683feebea6

--- src/com/android/mms/ui/NumberPickerDialog.java
+++ src/com/android/mms/ui/NumberPickerDialog.java
@@ -24,7 +24,6 @@ import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.android.internal.widget.NumberPicker;
 
@@ -87,8 +86,9 @@ public class NumberPickerDialog extends AlertDialog implements OnClickListener {
 
         setTitle(title);
 
-        setButton(context.getText(R.string.set), this);
-        setButton2(context.getText(R.string.no), (OnClickListener) null);
+        setButton(DialogInterface.BUTTON_POSITIVE, context.getText(R.string.set), this);
+        setButton(DialogInterface.BUTTON_NEGATIVE, context.getText(R.string.no),
+                (OnClickListener) null);
 
         LayoutInflater inflater =
                 (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
