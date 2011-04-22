b973847f0ad21bcb4653ec42af4397ad207ae003
Ravindra Raj Mamadgi <rmamadgi@sta.samsung.com>
2011-04-21 18:31:57 -0700

    Changing MMS video default size
    change the maximum video attachment support for MMS from
    300k up to 950k
    
    Change-Id: Idbf780c5342f87493c8d52c24b1657ebc5eb057e

--- src/com/android/mms/MmsConfig.java
+++ src/com/android/mms/MmsConfig.java
@@ -42,7 +42,7 @@ public class MmsConfig {
      */
     private static boolean mTransIdEnabled = false;
     private static int mMmsEnabled = 1;                         // default to true
-    private static int mMaxMessageSize = 300 * 1024;            // default to 300k max size
+    private static int mMaxMessageSize = 950 * 1024;            // default to 950k max size
     private static String mUserAgent = DEFAULT_USER_AGENT;
     private static String mUaProfTagName = DEFAULT_HTTP_KEY_X_WAP_PROFILE;
     private static String mUaProfUrl = null;
