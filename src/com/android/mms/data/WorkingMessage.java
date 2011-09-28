f740b30a70cfdcad410f9eb85a9a4308c506c4ba
Blackbay Build <blackbay-build@intel.com>
2011-09-28 03:25:43 -0700

    Mms: Device crashes when sending message with attachment.
    Need to handle IllegalStateException in addition of the
    MMSException when creating draft MMS.
    
    Change-Id: Ieb1618872d7faa472a6fea08c1b3ef1ae0cde183
    Author: Jianli Zhang <jianli.zhang@borqs.com>
    Signed-off-by: Frederic Predon <frederic.predon@intel.com>
    Signed-off-by: Xiaokang Qin <xiaokang.qin@intel.com>
    Signed-off-by: Beare, Bruce J <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 7844

--- src/com/android/mms/data/WorkingMessage.java
+++ src/com/android/mms/data/WorkingMessage.java
@@ -1535,6 +1535,9 @@ public class WorkingMessage {
             return res;
         } catch (MmsException e) {
             return null;
+        } catch (IllegalStateException e) {
+            Log.e(TAG,"failed to create draft mms "+ e);
+            return null;
         }
     }
 
