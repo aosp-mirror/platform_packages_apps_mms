4e268daeda88400462b6cf56a30bf7547442e158
Suresh Koleti <skolet@codeaurora.org>
2013-01-25 19:23:51 -0800

    Mms: Show menu option to delete all messages
    Delete all messages option is not visible in "Manage SIM card messages"
    so unable to delete all SIM card messages at a time.
    Use invalidate menu options to show "Delete all messages" option.
    
    Change-Id: I77645f51bffe2ca9718f81483e93e3bf925f293a

--- src/com/android/mms/ui/ManageSimMessages.java
+++ src/com/android/mms/ui/ManageSimMessages.java
@@ -150,6 +150,8 @@ public class ManageSimMessages extends Activity
                 // Let user know the SIM is empty
                 updateState(SHOW_EMPTY);
             }
+            // Show option menu when query complete.
+            invalidateOptionsMenu();
         }
     }
 
