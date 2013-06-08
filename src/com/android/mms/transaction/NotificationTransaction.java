3312aa38b93e004d7d62c3ee5730d0027d3ad28a
Rakesh Pallerla <rakesh@codeaurora.org>
2013-06-08 16:50:31 +0530

    WIP Mms(MSIM): Add support for MSIM.
    Add multi SIM support to SMS and MMS
    - Send SMS/MMS on selected preferred subscription.
    - Display SUB information in message conversation.
    - In MT message extract SUB information.
    - Save SUB information in provider.
    
    MMS Auto Download Feature
    - new implemenation to carry out following
     operations seemlessly.
    - Activate MMS PDP on all subs and send/received
      MT MMS on all other sub.
    - Deactivate the MMS PDP and revert back to original
      sub post the successful MMS operation.
    
    Add prompt support for SMS
    - Add prompt option in SMS multi sim settings.
    - New iterface API to get SMS prompt is enabled or not.
    - Disply prompt while sending SMS, if user selects
     prompt mode in multi sim settings.
    
    Change-Id: I6484a39d6eb1cfd176f84ecb84200a23dd71a360

--- src/com/android/mms/transaction/NotificationTransaction.java
+++ src/com/android/mms/transaction/NotificationTransaction.java
@@ -29,6 +29,7 @@ import java.io.IOException;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SqliteWrapper;
 import android.net.Uri;
 import android.provider.Telephony.Mms;
@@ -187,8 +188,22 @@ public class NotificationTransaction extends Transaction implements Runnable {
                             MessagingPreferenceActivity.getIsGroupMmsEnabled(mContext), null);
 
                     // Use local time instead of PDU time
-                    ContentValues values = new ContentValues(1);
+                    ContentValues values = new ContentValues(2);
                     values.put(Mms.DATE, System.currentTimeMillis() / 1000L);
+                    Cursor c = mContext.getContentResolver().query(mUri,
+                            null, null, null, null);
+                    if (c != null) {
+                        try {
+                            if (c.moveToFirst()) {
+                                int subId = c.getInt(c.getColumnIndex(Mms.SUB_ID));
+                                values.put(Mms.SUB_ID, subId);
+                            }
+                        } catch (Exception ex) {
+                            Log.e(TAG, "Exception:" + ex);
+                        } finally {
+                            c.close();
+                        }
+                    }
                     SqliteWrapper.update(mContext, mContext.getContentResolver(),
                             uri, values, null, null);
 
