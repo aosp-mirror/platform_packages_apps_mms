d738e3f3280320ff48219498f3dda773651d4395
Chris Lei <chrislei@codeaurora.org>
2013-07-31 15:34:49 -0700

    Mms: Correct Mms Transaction id handling.
    - The transaction id should come from the content location
    - Code should detect duplicate transactions solely based on
      mId. For example, code should detect when a retrieve transaction
      and a notification transacation both operation on the same mID.
    
    Bug: 10655376
    Change-Id: I3fefcc284ef8f75daacdef530ac19e1577ae73bb

--- src/com/android/mms/transaction/NotificationTransaction.java
+++ src/com/android/mms/transaction/NotificationTransaction.java
@@ -92,8 +92,9 @@ public class NotificationTransaction extends Transaction implements Runnable {
             throw new IllegalArgumentException();
         }
 
-        mId = new String(mNotificationInd.getTransactionId());
         mContentLocation = new String(mNotificationInd.getContentLocation());
+        mId = mContentLocation;
+        Log.d(TAG, "mId=" + mId);
 
         // Attach the transaction to the instance of RetryScheduler.
         attach(RetryScheduler.getInstance(context));
@@ -119,7 +120,8 @@ public class NotificationTransaction extends Transaction implements Runnable {
         }
 
         mNotificationInd = ind;
-        mId = new String(ind.getTransactionId());
+        mId = new String(mNotificationInd.getContentLocation());
+        Log.d(TAG, "mId=" + mId);
     }
 
     /*
