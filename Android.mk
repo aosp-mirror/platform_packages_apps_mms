3d393d40748486ca12f7959a095d3ac1aa2a8c45
Scott Kennedy <skennedy@google.com>
2014-03-26 13:50:00 -0700

    Point to new chips location
    Change-Id: I726e64f12b11834d8067e90829aecc88886f2483

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 # Include res dir from chips
-chips_dir := ../../../frameworks/ex/chips/res
+chips_dir := ../../../frameworks/opt/chips/res
 res_dirs := $(chips_dir) res
 
 $(shell rm -f $(LOCAL_PATH)/chips)
@@ -19,7 +19,7 @@ LOCAL_PACKAGE_NAME := Mms
 
 LOCAL_JAVA_LIBRARIES += telephony-common mms-common
 LOCAL_STATIC_JAVA_LIBRARIES += android-common jsr305
-LOCAL_STATIC_JAVA_LIBRARIES += android-common-chips
+LOCAL_STATIC_JAVA_LIBRARIES += libchips
 
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 LOCAL_AAPT_FLAGS := --auto-add-overlay
