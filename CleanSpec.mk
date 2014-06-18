cd201f482306850f701c3f5ac117c0ee86f74370
Jeff Sharkey <jsharkey@android.com>
2014-06-18 15:42:29 -0700

    CleanSpec for Mms.
    Change-Id: I94cfe85977878cdbe913ee2963fbaaaf388869b7

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -44,6 +44,9 @@
 #$(call add-clean-step, find $(OUT_DIR) -type f -name "IGTalkSession*" -print0 | xargs -0 rm -f)
 #$(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
 
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/Mms*)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/product/*/obj/APPS/Mms*)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
